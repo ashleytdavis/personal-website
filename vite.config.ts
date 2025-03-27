839b2601af245738038fce3ff8a191e4abc2d414
Ashley Davis <ashley921davis@gmail.com>
2025-03-27 11:31:44 -0400

    package updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/ashleytdavis-site/",
+  assetsInclude: [
+    "**/*.svg",
+    "**/*.png",
+    "**/*.jpg",
+    "**/*.mp3",
+    "**/*.wav",
+    "**/*.m43",
+    "**/*.jpeg",
+    "**/*.gif",
+    "**/*.webp",
+  ],
 });
